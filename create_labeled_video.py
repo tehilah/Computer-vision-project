bc0f0695d9b24788ac22a3a169f14bcd6227199b
Tehilah <tehilah.nagar@mail.huji.ac.il>
2019-06-26 17:07:21 +0300

    added train.py, new model

--- create_labeled_video.py
+++ create_labeled_video.py
@@ -1,6 +1,7 @@
 import cv2     # for capturing videos
 import math   # for mathematical operations
 from PIL import Image, ImageFont, ImageDraw, ImageEnhance
+from Classifier import recreate_model, predict_animal
 # import matplotlib.pyplot as plt    # for plotting the images
 # # %matplotlib inline
 # import pandas as pd
@@ -11,6 +12,8 @@ from PIL import Image, ImageFont, ImageDraw, ImageEnhance
 
 
 def convert_to_frames():
+	if not cv2.os.path.exists("images"):
+		cv2.os.makedirs("images")
 	count = 0
 	video_file = "national_dog_show.mp4"
 	cap = cv2.VideoCapture(video_file)  # capturing the video from the given path
@@ -21,29 +24,32 @@ def convert_to_frames():
 		if ret is not True:
 			break
 		if frame_id % math.floor(frame_rate) == 0:
-			filename = "frame%d.jpg" % count
+			filename = "%d.jpg" % count
 			count += 1
-			cv2.imwrite(filename, frame)
+			cv2.imwrite("images/"+filename, frame)
 	cap.release()
 	print("Done!")
 	return count
 
-# convert_to_frames()
-def label_frame(frame, i):
-	img = Image.open(frame)
+
+def label_frame(model, frame, i):
+	full_path = "images/"+frame
+	img = Image.open(full_path)
 	draw = ImageDraw.Draw(img)
-	# font = ImageFont.truetype(<font-file>, <font-size>)
 	font = ImageFont.truetype("TIMESS.ttf", 100)
-	# draw.text((x, y),"Sample Text",(r,g,b))
-	draw.text((150, 150), "Labrador", (255, 255, 255), font=font)
+	predicted_animal = predict_animal(full_path, model)
+	draw.text((150, 150), predicted_animal, (255, 255, 255), font=font)
+	img.save('labels/'+str(i)+'.jpg')
+
+
+def predict_frames():
+	dog_frames = cv2.os.listdir("images")
+	dog_frames.sort()
+	model = recreate_model()
 	if not cv2.os.path.exists("labels"):
 		cv2.os.makedirs("labels")
-	img.save('labels/labeled_frame'+str(i)+'.jpg')
-
-label_frame("frame0.jpg", 0)
-def predict_frames(num_frames):
-	for i in range(num_frames):
-		label_frame("frame"+str(i)+'.jpg', i)
+	for i, frame in enumerate(dog_frames):
+		label_frame(model, frame, i)
 
 def create_video():
 	image_folder = 'labels'
@@ -54,9 +60,12 @@ def create_video():
 	height, width, layers = frame.shape
 
 	video = cv2.VideoWriter(video_name, 0, 1, (width, height))
-
+	images.sort()
 	for image in images:
 		video.write(cv2.imread(cv2.os.path.join(image_folder, image)))
 
 	cv2.destroyAllWindows()
 	video.release()
+
+if __name__ == '__main__':
+	convert_to_frames()
